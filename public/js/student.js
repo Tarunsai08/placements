3063230463bb716ba75cbe538e08e7f95f6b4297
Tarun varaha sai <dtarunsai08@gmail.com>
2023-10-06 22:39:43 +0530

    integrated student page with backend

--- public/js/student.js
+++ public/js/student.js
@@ -9,4 +9,27 @@ axios.get('http://localhost:5000/api/student/get-student-result', {
 }).then((response) => {
     const arr = Array.from(response.data);
     console.log(arr);
+    const container = document.getElementById("cardsContainer");
+
+      arr.forEach((item) => {
+        // Create card div
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <h3>Placement Result</h3>
+            <p>Selected</p>
+            <h3>Company</h3>
+            <p>${item.company}</p>
+            <h3>LPA</h3>
+            <p>${item.package}LPA</p>
+            <h3>Batch</h3>
+            <p>${item.batch}</p>
+         `;
+
+        // Create heart icon here
+        console.log(item.package, item.company, item.name, item.batch);
+
+        container.appendChild(card);
+      });
 })
